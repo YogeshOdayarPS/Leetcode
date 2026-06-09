63cc1d10a3eb4c68ce6e123d7fd5c696741d07d8
YogeshOdayarPS <yogeshodayarps@gmail.com>
2026-06-08 20:03:42 -0700

    Time: 1 ms (71.91%) | Memory: 42.7 MB (90.82%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -2,7 +2,7 @@ class Solution {
     public String interpret(String command) {
         command=command.replace("(al)","al");
         command=command.replace("()","o");
-        command=command.replace("(al)","al");
+      
 
 
         return command;
