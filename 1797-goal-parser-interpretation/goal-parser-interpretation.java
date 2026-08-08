88de33e0826a0c6d0616149f8a17978c2a1a3ac2
YogeshOdayarPS <yogeshodayarps@gmail.com>
2026-08-08 07:03:30 -0700

    Time: 1 ms (74.98%) | Memory: 43 MB (52.11%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -2,9 +2,6 @@ class Solution {
     public String interpret(String command) {
         command=command.replace("(al)","al");
         command=command.replace("()","o");
-      
-
-
         return command;
     }
 }
