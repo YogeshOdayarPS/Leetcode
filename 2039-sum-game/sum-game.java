4cf6c7884761d8edb42b078b024a6c50d2be0e72
YogeshOdayarPS <yogeshodayarps@gmail.com>
2026-08-22 23:41:13 -0700

    Time: 8 ms (53.28%) | Memory: 47.1 MB (41.61%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -4,18 +4,18 @@ class Solution {
         int sum = 0;
         int q = 0;
 
-        for (int i=0;i <n/2;i++) {
-            if (num.charAt(i)== '?')
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?')
                 q++;
             else
-                sum += num.charAt(i)-'0';
+                sum += num.charAt(i) - '0';
         }
 
         for (int i = n / 2; i < n; i++) {
-            if (num.charAt(i) =='?')
+            if (num.charAt(i) == '?')
                 q--;
             else
-                sum -= num.charAt(i)-'0';
+                sum -= num.charAt(i) - '0';
         }
 
         if (q % 2 != 0)
